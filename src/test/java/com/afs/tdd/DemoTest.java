f6603a120fac52d09a43bd7bf46339cef55a8da8
LEECH3 <leech3@oocl.com>
2025-09-05 15:35:11 +0800

    Feat: add MarsRover class & test case

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,24 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
-class DemoTest {
-    @Test
-    void write_your_first_test() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class DemoTest {
+
+    @Test
+    public void should_return_y_plus_1_when_0_0_N_and_command_W() {
+        // given
+        String command = "W";
+        int x = 0;
+        int y = 0;
+        MarsRover marsRover = new MarsRover(x, y, "N");
+
+        // when
+        marsRover.executeCommand(command);
+
+        // then
+        int expectedY = 1;
+        assertEquals(expectedY, marsRover.getY());
     }
+
 }
